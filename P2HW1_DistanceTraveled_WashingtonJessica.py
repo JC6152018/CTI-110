271141e99e21c4a49550f01a2287ac15427c47df
JC6152018 <40345874+JC6152018@users.noreply.github.com>
2018-06-17 20:55:16 -0400

    Add files via upload
    P2HW1

---
+++ P2HW1_DistanceTraveled_WashingtonJessica.py
@@ -0,0 +1,25 @@
+# CTI-110
+# P2HW1 - Distance Traveled
+# Jessica Washington
+# June 17th 2018
+
+# Show the distance traveled over 6 hrs
+
+speed = 70
+distanceAfter6 = speed * 6 
+print('The distance a car travels in 6 hours is:')
+print (speed,'mph','* 6 hours =', distanceAfter6,"miles")
+
+#Show the distance traveled over 10 hours
+
+speed = 70
+distanceAfter10 = speed * 10
+print ('The distance a car travels in 10 hours is:')
+print (speed,'mph','* 10 hours =', distanceAfter10,"miles")
+
+#Show the distance traveled over 15 hours
+
+speed = 70
+distanceAfter15 = speed * 15
+print ('The distance a car travels in 15 hours is:')
+print (speed,'mph','* 15 hours =', distanceAfter15,"miles")
